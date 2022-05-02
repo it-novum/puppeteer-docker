f06ba48c60d545c11ac8e47e962ab1a4f56557f0
nook24 <info@nook24.eu>
2022-05-02 11:21:01 +0200

    Fix date format Catch errors Reformat code

--- src/webservices.js
+++ src/webservices.js
@@ -91,42 +91,46 @@
  
  
  app.post('/area_chart', async function(request, response){
-     //console.log(request.body);     // json data
-     var requestBody = request.body;
-     //console.log(util.inspect(requestBody, false, null, true /* enable colors */))
+     try{
+         //console.log(request.body);     // json data
+         var requestBody = request.body;
+         //console.log(util.inspect(requestBody, false, null, true /* enable colors */))
  
  
-     // Create new Chrome Browser
-     // --no-sandbox is bad and unsecure, but this is running inside of a Docker Container and only
-     // rendering our own trusted HTML so we don't really need to care about security anyway
-     const browser = await puppeteer.launch({
-         args: [
-             '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage',
-             //'--disable-web-security', '--font-render-hinting=none', '--headless', '--force-color-profile=srgb'
-         ],
-     });
+         // Create new Chrome Browser
+         // --no-sandbox is bad and unsecure, but this is running inside of a Docker Container and only
+         // rendering our own trusted HTML so we don't really need to care about security anyway
+         const browser = await puppeteer.launch({
+             args: [
+                 '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage',
+                 //'--disable-web-security', '--font-render-hinting=none', '--headless', '--force-color-profile=srgb'
+             ],
+         });
  
  
-     const page = await browser.newPage();
+         const page = await browser.newPage();
  
-     await page.goto('file:///src/area_chart_template.html', {
-         waitUntil: 'networkidle0', //networkidle2
-     });
+         await page.goto('file:///src/area_chart_template.html', {
+             waitUntil: 'networkidle0', //networkidle2
+         });
  
-     // Pass requestBody to area_chart_template.html and call renderChart() function of area_chart_template.html
-     await page.evaluate((requestBody) => {
-         window.renderChart(requestBody)
-     }, requestBody); // 1. pass variable as an argument
+         // Pass requestBody to area_chart_template.html and call renderChart() function of area_chart_template.html
+         await page.evaluate((requestBody) => {
+             window.renderChart(requestBody)
+         }, requestBody); // 1. pass variable as an argument
  
  
-     const chartDiv = await page.$('#chart')
-     const pngBuffer = await chartDiv.screenshot();
+         const chartDiv = await page.$('#chart')
+         const pngBuffer = await chartDiv.screenshot();
  
-     await browser.close();
+         await browser.close();
  
-     response.writeHead(200, {'Content-Type': 'image/png'});
-     response.end(pngBuffer, 'binary');
- 
+         response.writeHead(200, {'Content-Type': 'image/png'});
+         response.end(pngBuffer, 'binary');
+     }catch(e){
+         console.error(e);
+     }
+     return response;
  });
  
  // Start the Web Server
